368cf822bb418d6c1612623144807959db4ba463
zhe62 <zhe@kuvasystems.com>
2024-07-05 16:16:55 -0600

    added data error

--- docs/utils.js
+++ docs/utils.js
@@ -101,7 +101,7 @@ function drawScatter(input_data, label_array, div_id, title)
 	var color = d3.scaleOrdinal()
 		.range(["#EFB605", "#E58903", "#E01A25", "#C20049", "#991C71", "#66489F", "#2074A0", "#10A66E", "#7EB852"])
 		.domain(["fpga status error", "swirscan error", "darkcal error", " motor error", 
-				 "voltage error", "data lost error", "TEC error", "panhome error",  "restart"]);
+				 "voltage error", "data error", "TEC error", "panhome error",  "restart"]);
 
 	// append the svg object to the body of the page
 	var svg = d3.select(div_id)
@@ -383,6 +383,7 @@ function d3ParseFile(){
 	var fpgastatus_err_cnt = 0;
 	var volt_err_cnt = 0;
 	var tec_err_cnt = 0;
+	var data_err_cnt = 0;
 	var total_events_cnt = 0;
 	console.log("date_cur:"+date_cur)
 	realEventsData = [];
@@ -409,6 +410,8 @@ function d3ParseFile(){
 
 			if(data[i].Subject=="FPGA" && data[i].Verb == "find" && data[i].Object=="tec_temperature" && data[i].Status=="error")
 				tec_err_cnt++;
+			if( data[i].Object=="data" && data[i].Status=="error")
+				data_err_cnt++;
 		} else
 		{
 			if(restart_cnt!=0)
@@ -440,6 +443,10 @@ function d3ParseFile(){
 			{
 				realEventsData.push({"eventClass":"TEC error", "rate":tec_err_cnt/total_events_cnt, "date":date_cur, "count":tec_err_cnt});
 			}
+			if(data_err_cnt!=0)
+			{
+				realEventsData.push({"eventClass":"data error", "rate":data_err_cnt/total_events_cnt, "date":date_cur, "count":data_err_cnt});
+			}
 
 			
 			date_cur = data[i].RowKey.substring(0,10);
@@ -449,6 +456,7 @@ function d3ParseFile(){
 			fpgastatus_err_cnt = 0;
 			volt_err_cnt = 0;
 			tec_err_cnt = 0;
+			data_err_cnt = 0;
 			total_events_cnt = 0;
 
 		}
